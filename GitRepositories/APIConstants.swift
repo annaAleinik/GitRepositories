d4958672445172a70865c795d6d2753904bb0316
annaAleinik <annaaleinik1807@gmail.com>
2018-01-24 21:42:47 +0200

    update cart file, added ApiConstants class

---
+++ GitRepositories/APIConstants.swift
@@ -0,0 +1,15 @@
+//
+//  APIConstants.swift
+//  GitRepositories
+//
+//  Created by Анюта on 24.01.18.
+//  Copyright © 2018 Анюта. All rights reserved.
+//
+
+import Foundation
+
+//Example of api link for list of repositories
+//<https://api.github.com/repositories?since=364>; rel="next"
+
+let baseURL = "https://api.github.com"
+let allRepositoriesURL = "/repositories"
---
+++ GitRepositories/GRNetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  GRNetworkManager.swift
+//  GitRepositories
+//
+//  Created by Анюта on 24.01.18.
+//  Copyright © 2018 Анюта. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class NetworkManager {
+    
+}
